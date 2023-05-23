80f47f305e9e02910a94637125f03a42f4c1c38d
mbriseno <miiguelb07@gmail.com>
2023-05-22 21:30:30 -0600

    OnTriggerEnter2D function added

--- Car Game/Assets/Collision.cs
+++ Car Game/Assets/Collision.cs
@@ -7,4 +7,8 @@ public class Collision : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Car crashed!");
     }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Car entered pink zone!");
+    }
 }
