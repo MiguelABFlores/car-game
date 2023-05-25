fc1d0f035091c14384bc2368e6a805cfbcf0f814
Miguel Angel Briseño Flores <miiguelb07@gmail.com>
2023-05-24 19:56:12 -0600

    object destroy and one package added

--- Car Game/Assets/Delivery.cs
+++ Car Game/Assets/Delivery.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] float destroyDelay = 0.5f;
     bool hasPackage = false;
     void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == "Package" && hasPackage == false){
+        if (other.tag == "Package" && !hasPackage){
             Debug.Log("Package picked up.");
             hasPackage = true;
+            Destroy(other.gameObject, destroyDelay);
         }
 
         if (other.tag == "Customer" && hasPackage){
