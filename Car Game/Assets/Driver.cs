489eadd2c3cb41b695f297ee4515ee549131ddce
Miguel Angel Briseño Flores <miiguelb07@gmail.com>
2023-05-24 22:10:34 -0600

    booster and bump added

--- Car Game/Assets/Driver.cs
+++ Car Game/Assets/Driver.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float steerSpd = 1f;
-    [SerializeField] float moveSpd = 0.01f;
-    void Start()
-    {
-        
-    }
+    [SerializeField] float steerSpd = 150f;
+    [SerializeField] float moveSpd = 15f;
+    [SerializeField] float slowSpeed = 7f;
+    [SerializeField] float boostSpeed = 20f;
 
     void Update()
     {
         float steerVal = -Input.GetAxis("HAxis") * steerSpd * Time.deltaTime;
         float moveVal = Input.GetAxis("VAxis") * moveSpd * Time.deltaTime;
-        // Object Rotation
         transform.Rotate(0,0,steerVal);
-        // Object Translation
         transform.Translate(0,moveVal,0);
+    }
+
+    //When colliding reduce movement speed
+    void OnCollisionEnter2D(Collision2D other) {
+        moveSpd = slowSpeed;
+    }
+
+    //When picking booster increase movement speed
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Powerup"){
+            moveSpd = boostSpeed;
+        }
     }
 }
